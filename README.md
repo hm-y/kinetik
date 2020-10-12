c3acf3a8787cdc9a95c09b5ef530f2952520623e
Hilmi Murat Yildirim <hmy0hmy@gmail.com>
2020-10-11 22:11:23 -0400

    Organize the sections and the navbar link

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 *New website Prep*
   
 Template taken from: [uideck.com](https://uideck.com/templates/basic/)
+  
+Logo?
+Colors?
+spaces
+mobile
+clean code
+transfer to domain
--- assets/js/main.js
+++ assets/js/main.js
@@ -9,34 +9,34 @@ $(function () {
     
     //===== Sticky
 
-    $(window).on('scroll', function (event) {
-        var scroll = $(window).scrollTop();
-        if (scroll < 20) {
-            $(".navbar-area").removeClass("sticky");
-        } else {
-            $(".navbar-area").addClass("sticky");
-        }
-    });
+    // $(window).on('scroll', function (event) {
+    //     var scroll = $(window).scrollTop();
+    //     if (scroll < 20) {
+    //         $(".navbar-area").removeClass("sticky");
+    //     } else {
+    //         $(".navbar-area").addClass("sticky");
+    //     }
+    // });
 
 
 
     //===== Section Menu Active
 
-    var scrollLink = $('.page-scroll');
+    //var scrollLink = $('.page-scroll');
     // Active link switching
-    $(window).scroll(function () {
-        var scrollbarLocation = $(this).scrollTop();
+    // $(window).scroll(function () {
+    //     var scrollbarLocation = $(this).scrollTop();
 
-        scrollLink.each(function () {
+    //     scrollLink.each(function () {
 
-            var sectionOffset = $(this.hash).offset().top - 73;
+    //         var sectionOffset = $(this.hash).offset().top - 73;
 
-            if (sectionOffset <= scrollbarLocation) {
-                $(this).parent().addClass('active');
-                $(this).parent().siblings().removeClass('active');
-            }
-        });
-    });
+    //         if (sectionOffset <= scrollbarLocation) {
+    //             $(this).parent().addClass('active');
+    //             $(this).parent().siblings().removeClass('active');
+    //         }
+    //     });
+    // });
 
 
     //===== close navbar-collapse when a  clicked
